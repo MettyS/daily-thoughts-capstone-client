db01120114c29ab86aecc52a3c162a3e5a70f0fe
Metty Schroeder <mettyj.schroeder@gmail.com>
2020-12-22 13:52:41 -0500

    updating config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const defaultExport = {
-  API_ENDPOINT: process.env.API_ENDPOINT || 'https://enigmatic-river-49566.herokuapp.com'//'http://localhost:8000'
+  API_ENDPOINT: process.env.API_ENDPOINT || 'https://enigmatic-river-49566.herokuapp.com',//'http://localhost:8000'
+  API_KEY: process.env.REACT_APP_API_KEY || 'change-this'
 };
 
 export default defaultExport;
